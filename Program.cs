ead299cd116c3fea6fa6ec42bdaaa2eb255d3e4a
Anteste <reversodr@gmail.com>
2020-06-21 16:17:10 +0100

    Game Ready

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Say_My_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qui est tu ?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Bonjour " + name);
+            Console.WriteLine("Quel est ta date de naissance ? ");
+
+            string year = Console.ReadLine();
+            int yearNum = int.Parse(year);
+
+            int age = DateTime.Now.Year - yearNum;
+            Console.WriteLine("tu as: " + age.ToString());
+
+            if (age < 40)
+            {
+                Console.WriteLine("tu es encore jeune " + name);
+            }
+            else
+            {
+                Console.WriteLine("tu es vieux dis donc Monsieur " + name);
+            }
+
+            Console.ReadKey();       
+
+         
+            
+        }
+    }
+}
